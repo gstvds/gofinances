d97138920457be75701155a03180e10bc6dc0dd1
Gustavo Silva <gstvds@icloud.com>
2021-05-30 17:01:47 -0300

    feat: Adds Icons with expo vector-icons

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Container, Header, UserContainer, UserInfo, Photo, User, UserGreeting, UserName } from './styles';
+
+import { Container, Header, UserContainer, UserInfo, Photo, User, UserGreeting, UserName, Icon } from './styles';
 
 export function Dashboard() {
   return (
     <Container>
       <Header>
         <UserContainer>
-        <UserInfo>
-          <Photo source={{ uri: 'https://github.com/gstvds.png' }} />
-          <User>
-            <UserGreeting>Olá,</UserGreeting>
-            <UserName>Gustavo</UserName>
-          </User>
-        </UserInfo>
+          <UserInfo>
+            <Photo source={{ uri: 'https://github.com/gstvds.png' }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Gustavo</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
         </UserContainer>
       </Header>
     </Container>
